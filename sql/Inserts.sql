9feec50207b7e70e1210eb238622ebfb612c89f0
zschun <zschun@ws-inf-ge-04.bbc-zh.local>
2019-07-02 14:25:29 +0200

    Error page

--- sql/Inserts.sql
+++ sql/Inserts.sql
@@ -2,6 +2,7 @@ delete from conversation_access_key;
 delete from invite;
 delete from user_in_chat;
 delete from user_read_message;
+delete from email_verification_key;
 delete from message;
 delete from chat;
 delete from user;
